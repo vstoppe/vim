9b750bd97e720e8a77d38553037e8c2adb6d6a9f
Volker Stoppe <vst@lindenbox.de>
2023-07-21 22:39:57 +0200

    added gruvbox-lua, indent-blankline and autoclose

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,5 +1,9 @@
 -- Install Packer automatically if it's not installed(Bootstraping)
 -- Hint: string concatenation is done by `..`
+--
+-- Inspired by:
+-- https://martinlwx.github.io/en/config-neovim-from-scratch/
+--
 local ensure_packer = function()
     local fn = vim.fn
     local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
@@ -39,36 +43,32 @@ return require('packer').startup(function(use)
     -- NOTE: PUT YOUR THIRD PLUGIN HERE --
     ---------------------------------------
 
-	use 'morhetz/gruvbox'
+	use 'ellisonleao/gruvbox.nvim'
 
     -- Plugins for completions engine
-   
+
     use { 'williamboman/mason.nvim' }
     use { 'williamboman/mason-lspconfig.nvim'}
     use { 'neovim/nvim-lspconfig' }
-    use { 'hrsh7th/nvim-cmp', config = [[require('config.nvim-cmp')]] }    
-    use { 'hrsh7th/cmp-nvim-lsp', after = 'nvim-cmp' } 
+    use { 'hrsh7th/nvim-cmp', config = [[require('config.nvim-cmp')]] }
+    use { 'hrsh7th/cmp-nvim-lsp', after = 'nvim-cmp' }
     use { 'hrsh7th/cmp-buffer', after = 'nvim-cmp' }        -- buffer auto-completion
     use { 'hrsh7th/cmp-path', after = 'nvim-cmp' }          -- path auto-completion
     use { 'hrsh7th/cmp-cmdline', after = 'nvim-cmp' }       -- cmdline auto-completion
     use 'L3MON4D3/LuaSnip'
-    use 'saadparwaiz1/cmp_luasnip'  
-    
+    use 'saadparwaiz1/cmp_luasnip'
+
     -- Statusline / lua-line
     use({
         "nvim-lualine/lualine.nvim",
         event = "BufEnter",
         config = [[require("config.lualine")]],
-        requires = { "nvim-web-devicons", opt = true },
+        requires = { "nvim-web-devicons" },
     })
---    use ({ 
---        "nvim-lualine/lualine.nvim", 
---        requires = { 'nvim-tree/nvim-web-devicons', opt = true },
---        config = function()
---            require('config.lualine')
---        end,
---    })
-    
+
+    use 'm4xshen/autoclose.nvim'
+    use "lukas-reineke/indent-blankline.nvim"
+
 
 
 
