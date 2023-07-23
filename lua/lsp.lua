fd321e43983ccc3c60597606ce482f98ed6facd5
Volker Stoppe <vst@lindenbox.de>
2023-07-23 21:40:47 +0200

    configured language servers

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -38,16 +38,12 @@ local on_attach = function(client, bufnr)
     vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
 end
 
-lspconfig.pylsp.setup({
-  on_attach = on_attach,
-})
-
--- lspconfig.bashls.setup({
---   on_attach = on_attach,
--- })
-
+-- Setup the language servers
+lspconfig.pylsp.setup({ on_attach = on_attach, })
 require'lspconfig'.bashls.setup{}
 require('lspconfig').yamlls.setup{}
+require'lspconfig'.lua_ls.setup {}
+require('lspconfig').vimls.setup{}
 require'lspconfig'.rust_analyzer.setup{
   settings = {
     ['rust-analyzer'] = {
