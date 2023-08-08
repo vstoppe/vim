803dd4103da3a571a8b9423c824161879162d70c
vstoppe <vst@lindenbox.de>
2023-08-08 19:17:08 +0200

    Basic schema-registry config

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -41,7 +41,6 @@ end
 -- Setup the language servers
 lspconfig.pylsp.setup({ on_attach = on_attach, })
 require('lspconfig').bashls.setup{}
-require('lspconfig').yamlls.setup{}
 require('lspconfig').lua_ls.setup {}
 require('lspconfig').rnix.setup {}
 require('lspconfig').vimls.setup{}
@@ -53,6 +52,22 @@ require('lspconfig').rust_analyzer.setup{
       }
     }
   }
+}
+require('lspconfig').yamlls.setup{
+
+  settings = {
+    yaml = {
+      schemaStore = {
+        -- You must disable built-in schemaStore support if you want to use
+        -- this plugin and its advanced options like `ignore`.
+        enable = false,
+        -- Avoid TypeError: Cannot read properties of undefined (reading 'length')
+        url = "",
+      },
+      schemas = require('schemastore').yaml.schemas(),
+    },
+  },
+
 }
 
 -- Mason setup
