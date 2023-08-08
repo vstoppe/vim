b32111354dcdf9d47f6e363c18ccfa545bc50fa1
vstoppe <vst@lindenbox.de>
2023-08-08 19:11:14 +0200

    added schemastore-plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -57,6 +57,7 @@ return require('packer').startup(function(use)
   use { "hrsh7th/cmp-cmdline", after = 'nvim-cmp' }       -- cmdline auto-completion
   use { 'L3MON4D3/LuaSnip' }
   use { 'saadparwaiz1/cmp_luasnip' }
+  use { 'b0o/schemastore.nvim'}
 
   -- Statusline / lua-line
   use { "nvim-lualine/lualine.nvim",
